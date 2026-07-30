bdd08a379770f985ce27f9fb7735e5c5ca46084d
Julia <serganinaiuliia@gmail.com>
2026-07-30 18:09:23 +0400

    Расширенный фильтр каталога, футер, навигация, рабочий поиск

--- app/countries/[countryId]/page.tsx
+++ app/countries/[countryId]/page.tsx
@@ -1,15 +1,16 @@
 import { getCities } from '@/lib/queries/cities'
+import { getFilterReferenceData } from '@/lib/queries/referenceData'
 import { CityGrid } from '@/components/city/CityGrid'
 
 export default async function CountryPage() {
-  const cities = await getCities()
+  const [cities, reference] = await Promise.all([getCities(), getFilterReferenceData()])
 
   return (
     <main className="container mx-auto px-4 py-8">
       <h1 className="text-3xl font-bold mb-8">
         Найди свой город для релокации
       </h1>
-      <CityGrid cities={cities} />
+      <CityGrid cities={cities} reference={reference} />
     </main>
   )
 }
